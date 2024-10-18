4bab11c95fbc998e4022da67f64ef12f8b377b17
Wesley de Oliveira <wesleydeoliveirapereira95@gmail.com>
2024-10-18 14:03:19 -0300

    Ajuste no exemplo

--- ex4.py
+++ ex4.py
@@ -6,6 +6,8 @@
 #Exemplo(0+1=1, 1+3=4, 4+5=9, 9+7=16, 16+9=25, 25+11=36 logo 36+13=39!!!)
 
 #d)4,16,36,64, Resultado é 100, cada número é o quadrado de números pares!
+#Exemplo: 2x2=4, 4x4=16, 6x6=36, 8x8=64 10x10=100
+
 
 #e)1,1,2,3,5,8, Resultado é 13, sequência de Fibonacci!
 
